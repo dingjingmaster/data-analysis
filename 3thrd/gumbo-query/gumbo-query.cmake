fdc5566f2e6e54811289506356e4b7f1a64805af
dingjing <dingjing@live.cn>
2025-02-15 16:38:12 +0800

    [ADD] 整合gumbo-query

---
+++ 3thrd/gumbo-query/gumbo-query.cmake
@@ -0,0 +1,16 @@
+file(GLOB GUMBO_QUERY_SRC
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Document.cpp
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Document.h
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Node.cpp
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Node.h
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Object.cpp
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Object.h
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Parser.cpp
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Parser.h
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/QueryUtil.cpp
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/QueryUtil.h
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Selection.cpp
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Selection.h
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Selector.cpp
+        ${CMAKE_SOURCE_DIR}/3thrd/gumbo-query/src/Selector.h
+)
